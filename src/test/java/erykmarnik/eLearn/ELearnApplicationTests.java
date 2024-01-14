4b502c56203cb0750c38fdb6d80c9f1df980a69f
erykov1 <erykov1@gmail.com>
2024-01-14 12:53:18 +0100

    init

---
+++ src/test/java/erykmarnik/eLearn/ELearnApplicationTests.java
@@ -0,0 +1,13 @@
+package erykmarnik.eLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
