3fc16c56a7654b20ef701e58178210497aeb3e3d
erykov1 <erykov1@gmail.com>
2024-04-24 18:02:27 +0200

    enable scheduler and json creator for news enrollment

--- src/main/java/erykmarnik/eLearn/ELearnApplication.java
+++ src/main/java/erykmarnik/eLearn/ELearnApplication.java
@@ -1,12 +1,18 @@
 package erykmarnik.eLearn;
 
 import erykmarnik.eLearn.security.RsaKeyProperties;
+import erykmarnik.eLearn.user.domain.UserFacade;
+import erykmarnik.eLearn.user.dto.CreateUserDto;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
+@EnableScheduling
 public class ELearnApplication {
 
 	public static void main(String[] args) {
--- src/main/java/erykmarnik/eLearn/notification/dto/EnrollForNewsDto.java
+++ src/main/java/erykmarnik/eLearn/notification/dto/EnrollForNewsDto.java
@@ -1,13 +1,18 @@
 package erykmarnik.eLearn.notification.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-@AllArgsConstructor
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EnrollForNewsDto {
   String userMail;
+
+  @JsonCreator
+  public EnrollForNewsDto(@JsonProperty("userMail") String userMail) {
+    this.userMail = userMail;
+  }
 }
